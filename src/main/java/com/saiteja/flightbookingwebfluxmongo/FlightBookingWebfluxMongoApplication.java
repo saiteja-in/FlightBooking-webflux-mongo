22bf7a5c6fd6dab891261c3a13ae722f765755a5
saiteja-in <vurukondasaiteja13@gmail.com>
2025-11-23 01:02:27 +0530

    setup dependencies

--- src/main/java/com/saiteja/flightbookingwebfluxmongo/FlightBookingWebfluxMongoApplication.java
+++ src/main/java/com/saiteja/flightbookingwebfluxmongo/FlightBookingWebfluxMongoApplication.java
@@ -2,8 +2,10 @@ package com.saiteja.flightbookingwebfluxmongo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.reactive.config.EnableWebFlux;
 
 @SpringBootApplication
+@EnableWebFlux
 public class FlightBookingWebfluxMongoApplication {
 
     public static void main(String[] args) {
