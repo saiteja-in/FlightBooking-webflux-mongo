0fd4ec711e3acf22eb61d5b786f03df0e384b814
saiteja-in <vurukondasaiteja13@gmail.com>
2025-11-22 23:58:14 +0530

    project init

---
+++ src/main/java/com/saiteja/flightbookingwebfluxmongo/FlightBookingWebfluxMongoApplication.java
@@ -0,0 +1,13 @@
+package com.saiteja.flightbookingwebfluxmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightBookingWebfluxMongoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlightBookingWebfluxMongoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/saiteja/flightbookingwebfluxmongo/FlightBookingWebfluxMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saiteja.flightbookingwebfluxmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightBookingWebfluxMongoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
