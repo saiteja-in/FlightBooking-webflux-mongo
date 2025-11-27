66beeff75e2247ddb09a4322751d2da049af5bca
saiteja-in <vurukondasaiteja13@gmail.com>
2025-11-27 09:49:54 +0530

    fix

--- src/main/java/com/saiteja/flightbookingwebfluxmongo/config/MongoAuditConfig.java
+++
@@ -1,9 +0,0 @@
-package com.saiteja.flightbookingwebfluxmongo.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.config.EnableReactiveMongoAuditing;
-
-@Configuration
-@EnableReactiveMongoAuditing
-public class MongoAuditConfig {
-}
